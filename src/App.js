f6f14b6faccef19a860bf321e3754e878aa6277d
HenryCS <henryca8@hotmail.com>
2024-08-19 00:03:15 -0500

    Actualizar título de página/useEffect

--- src/App.js
+++ src/App.js
@@ -1,32 +1,20 @@
 import './App.css';
-import { useState } from 'react';
-import ListadoResultados from './ListadoResultados';
-import FormularioNumeros from './FormularioNumeros';
+import { useState, useEffect } from 'react';
 function App() {  
-  const [operaciones, setOperacion] = useState([]);
+  const [texto, setTexto] = useState("");
 
-  function sumar(event){
-    event.preventDefault();
-    const valor1 = parseInt(event.target.valor1.value);
-    const valor2 = parseInt(event.target.valor2.value);
-    const resultado = valor1 + valor2;
-    const nuevo = {
-      respuesta: resultado,
-      val1: valor1,
-      val2: valor2
-    }
+  useEffect(() => {document.title = texto},[texto])
 
-    setOperacion([nuevo,...operaciones]);//IMPORTANTE LOS PUNTOS SUSPNSIVOS
-
-    event.target.valor1.value = '';
-    event.target.valor2.value = '';
+  function cambiar(e){
+    setTexto(e.target.value);
   }
-
 
   return (
     <div>
-      <FormularioNumeros onSumar={sumar}/>
-      <ListadoResultados resultados={operaciones}/>
+      <p>
+        <input type="text" onChange = {cambiar}/>
+        <p>{texto}</p>
+      </p>
     </div>
   );
 }
