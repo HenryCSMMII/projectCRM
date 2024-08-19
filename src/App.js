86c20b43394f89e60723ea18951922d9f63223f9
HenryCS <henryca8@hotmail.com>
2024-08-19 00:56:10 -0500

    Formulario con hooks

--- src/App.js
+++ src/App.js
@@ -1,16 +1,57 @@
 import './App.css';
 import { useState, useEffect } from 'react';
-import CoordenadaFlecha from './CoordenadaFlecha';
 function App() {  
-  const [visible, setVisible] = useState(true);
-  function ocultar(){
-    setVisible(false);
+  const [datos, setDatos] = useState({
+    nombre: '',
+    edad: '',
+    estudios: false
+  });
+
+  function cambiarNombre(e){
+    setDatos((valores) => ({
+      ...valores,
+      nombre: e.target.value
+    }))
+  }
+
+  function cambiarEdad(e){
+    setDatos((valores) => ({
+      ...valores,
+      edad: e.target.value
+    }))
+  }
+  
+  function cambiarEstudios(e){
+    setDatos((valores) => ({
+      ...valores,
+      estudios: !datos.estudios
+    }))
+  }
+
+  function procesar(e){
+    e.preventDefault();
+    alert("Datos cargados: "+ datos.nombre + " " + datos.edad + " " + datos.estudios);
   }
 
   return (
     <div>
-      {visible ? <CoordenadaFlecha /> : <p>Se ocultó la coordenada</p>}
-      <button onClick={ocultar}>Ocultar</button>
+      <form onSubmit={procesar}>
+        <p>Ingrese su nombre:
+          <input type="text" placeholder="Nombre" value={datos.nombre} onChange={cambiarNombre} />
+        </p>
+        <p>Ingrese su edad:
+          <input type="text" placeholder="Edad" value={datos.edad} onChange={cambiarEdad} />
+        </p>
+        <p>Estudios:
+          <input type="checkbox" checked={datos.estudios} onChange={cambiarEstudios} />
+        </p>
+        <button type="submit">Enviar</button>
+      </form>
+      <hr/>
+      <h3>Datos ingresados</h3>
+      <p>Nombre: {datos.nombre}</p>
+      <p>Edad: {datos.edad}</p> 
+      <p>Estudios: {datos.estudios ? "Si" : "No"}</p>
     </div>
   );
 }
