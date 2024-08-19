146cb0189d723fa71652446b2f5004144753d67d
HenryCS <henryca8@hotmail.com>
2024-08-18 20:31:29 -0500

    Use state

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-
-function realizarOperacion(e){
-  e.preventDefault();
-  const valor1 = parseInt(e.target.valor1.value);
-  const valor2 = parseInt(e.target.valor2.value);
-  const suma = valor1 + valor2;
-  alert('La suma es: ' + suma);
-}
+import { useState } from 'react';
 
 function App() {
-
+  function generarAleatorio(){
+    const valor = Math.trunc(Math.random() * 10);
+    setValor(valor);
+  }
+  
+  const [numero, setValor] = useState(0);
+  //CUANDO SE COLOCA ONCLICK NO RECIBE PARÁMETRO
   return (
     <div>
-      <form onSubmit={realizarOperacion}>
-        <p>Ingrese el primer valor:
-          <input type="number" name="valor1"/>
-        </p>
-        <p>Ingrese el segundo valor:
-          <input type="number" name="valor2"/>
-        </p>
-        <p>
-          <input type="submit" name="sumar"/>
-        </p>
-      </form>
+      <p>Número aleatorio: {numero}</p>
+      
+      <button onClick={generarAleatorio()}>
+        Generar valor aleatorio
+      </button>
     </div>
   );
 }
