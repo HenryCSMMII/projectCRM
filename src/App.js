d935bfc28f6b4de8f6d5fd9def24f645a29df27e
HenryCS <henryca8@hotmail.com>
2024-08-19 18:30:23 -0500

    Aplicación CRUD de clientes

--- src/App.js
+++ src/App.js
@@ -1,50 +1,28 @@
 import './App.css';
 import { useState, useEffect } from 'react';
-import { rubros, articulos } from './datos.js';
+import ListClientesComponent from './components/ListClientesComponent';
+import AddClienteComponent from './components/AddClienteComponent';
+import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 function App() {  
-  const [rubro, setRubro] = useState(rubros[0]);
-  const [articulosRubro, setArticuloRubro] = useState(articulos.filter(articulo => articulo.codigorubro === rubro.codigo));
-
-  const [articulo, setArticulo] = useState(articulosRubro[0]);
-
-  function cambiarRubro(e){
-    setRubro(rubros.find(rubro => rubro.codigo === parseInt(e.target.value)));
-    const articulosRubro = articulos.filter(articulo => articulo.codigorubro == parseInt(e.target.value));
-    setArticuloRubro(articulosRubro);
-    setArticulo(articulosRubro[0]);
-  }
-
-  function cambiarArticulo(e){
-    setArticulo(articulosRubro.find(articulo => articulo.codigo === parseInt(e.target.value)));
-  }
+  
 
   return (
-    <div className="formulario">
-      <div>
-        <select value={rubro.codigo} onChange={cambiarRubro}>
-          {rubros.map(rubro => (
-            <option key={rubro.codigo} value={rubro.codigo}>
-              {rubro.nombre}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div>
-        <select value={articulo.codigo} onChange={cambiarArticulo}>
-          {articulosRubro.map(articulo => (
-            <option key={articulo.codigo} value={articulo.codigo}>
-              {articulo.nombre}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div>
-        <ul>
-          <li>Rubro: <strong>{rubro.nombre}</strong></li>
-          <li>Articulo: <strong>{articulo.nombre}</strong></li>
-          <li>Precio: <strong>{articulo.precio}</strong></li>
-        </ul>
-      </div>
+    <div>
+      <BrowserRouter>
+        <HeaderComponent />
+        <div className="container">
+          <Routes>
+            <Route exact path='/' element={<ListClientesComponent />}></Route>
+            <Route exact path='/clientes' element={<ListClientesComponent />}></Route>
+            <Route exact path='/add-cliente' element={<AddClienteComponent />}></Route>
+            <Route exact path='/edit-cliente/:id' element={<AddClienteComponent />}></Route>
+            </Routes>
+        </div>
+        <FooterComponent />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/FooterComponent.js
@@ -0,0 +1,14 @@
+import React from "react";
+export const FooterComponent = () => {
+    return (
+        <div>
+            <footer className="footer">
+                <span className="text-muted">
+                    Todos los derechos reservados 2021 @Proyecto001
+                </span>
+            </footer>
+        </div>
+    );
+}
+
+export default FooterComponent;
