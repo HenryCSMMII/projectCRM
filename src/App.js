0f6c0ca98cfa1ffbdb02652a1b0ad913c0c971f8
HenryCS <henryca8@hotmail.com>
2024-08-18 22:48:37 -0500

    Contador con botones

--- src/App.js
+++ src/App.js
@@ -2,27 +2,21 @@ import './App.css';
 import { useState } from 'react';
 import Dado from './Dado';
 
-function App() {
-
-  function generarValor(){
-    return Math.trunc(Math.random()*6);
-  }
-  function tirarDados(){
-    setValor1(generarValor());
-    setValor2(generarValor());
-    setValor3(generarValor());
+function App() {  
+  function aumentarContador(){
+    if(contador >= 10){
+      alert("Reinicio");
+      setContador(0);
+    }
+    else setContador(contador+1);
   }
 
-  const [valor1,setValor1] = useState(0);
-  const [valor2,setValor2] = useState(0);
-  const [valor3,setValor3] = useState(0);  
+  const [contador,setContador] = useState(0);
 
   return (
     <div>
-      <Dado valor={valor1}/>
-      <Dado valor={valor2}/>
-      <Dado valor={valor3}/>
-      <button onClick={tirarDados}>Tirar dados</button>
+      <Dado valor={contador}/>
+      <button onClick={aumentarContador}>Aumenta</button>
     </div>
   );
 }
