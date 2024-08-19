c91ce4fae9f0ca07d37bacc2f638430d343d456a
HenryCS <henryca8@hotmail.com>
2024-08-19 01:18:21 -0500

    Desplegables y radio buttons

--- src/App.js
+++ src/App.js
@@ -1,57 +1,42 @@
 import './App.css';
 import { useState, useEffect } from 'react';
 function App() {  
-  const [datos, setDatos] = useState({
-    nombre: '',
-    edad: '',
-    estudios: false
-  });
+  const [dia, setDia] = useState("Lunes");
+  const [equipo, setEquipo] = useState("Real Madrid");
 
-  function cambiarNombre(e){
-    setDatos((valores) => ({
-      ...valores,
-      nombre: e.target.value
-    }))
+  function cambiaDia(e){
+    setDia(e.target.value);
   }
 
-  function cambiarEdad(e){
-    setDatos((valores) => ({
-      ...valores,
-      edad: e.target.value
-    }))
-  }
-  
-  function cambiarEstudios(e){
-    setDatos((valores) => ({
-      ...valores,
-      estudios: !datos.estudios
-    }))
-  }
-
-  function procesar(e){
-    e.preventDefault();
-    alert("Datos cargados: "+ datos.nombre + " " + datos.edad + " " + datos.estudios);
+  function cambiaEquipo(e){
+    setEquipo(e.target.value);
   }
 
   return (
     <div>
-      <form onSubmit={procesar}>
-        <p>Ingrese su nombre:
-          <input type="text" placeholder="Nombre" value={datos.nombre} onChange={cambiarNombre} />
-        </p>
-        <p>Ingrese su edad:
-          <input type="text" placeholder="Edad" value={datos.edad} onChange={cambiarEdad} />
-        </p>
-        <p>Estudios:
-          <input type="checkbox" checked={datos.estudios} onChange={cambiarEstudios} />
-        </p>
-        <button type="submit">Enviar</button>
-      </form>
-      <hr/>
-      <h3>Datos ingresados</h3>
-      <p>Nombre: {datos.nombre}</p>
-      <p>Edad: {datos.edad}</p> 
-      <p>Estudios: {datos.estudios ? "Si" : "No"}</p>
+      <h1>Seleccionar día de la semana</h1>
+      <p>
+        <select valor={dia} onChange={cambiaDia}>
+          <option>Lunes</option>
+          <option>Martes</option>
+          <option>Miércoles</option>
+          <option>Jueves</option>
+          <option>Viernes</option>
+          <option>Sábado</option>
+          <option>Domingo</option>
+        </select>
+      </p>
+      <p>Día seleccionado: {dia}</p>
+      <h1>Seleccionar equipo de fútbol</h1>
+      <p>
+        <input type="radio" value="Liverpool" checked={equipo == "Liverpool"} onChange={cambiaEquipo}/>
+        Liverpool
+        <input type="radio" value="Ajax" checked={equipo == "Ajax"} onChange={cambiaEquipo}/>
+        Ajax
+        <input type="radio" value="Rennes" checked={equipo == "Rennes"} onChange={cambiaEquipo}/>
+        Rennes                
+      </p>
+      <p>Equipo seleccionado: {equipo}</p>
     </div>
   );
 }
