9c3944c390aff35174b38afe6d02cad993b97651
HenryCS <henryca8@hotmail.com>
2024-08-18 20:49:11 -0500

    Listado de articulos

--- src/App.js
+++ src/App.js
@@ -2,24 +2,56 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  function generarAleatorios(){
-    const vector = new Array(4);
-    for(let x = 0; x < vector.length; x++){
-      vector[x] = Math.trunc(Math.random()*10);
+
+  function eliminarUltimaFila(){
+    if(articulos.length > 0){
+      const temp = Array.from(articulos);
+      temp.pop();
+      setArticulos(temp);
     }
-    setNumeros(vector);
   }
-  
-  const [numeros, setNumeros] = useState([0,0,0,0]);
+
+  const [articulos, setArticulos] = useState([
+    {
+      codigo: 1,
+      descripcion: "Papas",
+      precio: 10.42
+    },
+    {
+      codigo: 2,
+      descripcion: "Naranjas",
+      precio: 11.21
+    },
+    {
+      codigo: 3,
+      descripcion: "Peras",
+      precio: 0.10
+    },
+  ]);
+
   return (
     <div>
-      <p>Números aleatorios:</p>
-      {numeros.map(num => (
-        <p>{num}</p>
-      ))}
-      <button onClick={generarAleatorios}>
-        Generar valores aleatorios
-      </button>
+      <table border="1">
+        <thead>
+          <tr>
+            <th>Código</th>
+            <th>Descripción</th>
+            <th>Precio</th>
+          </tr>
+        </thead>
+        <tbody>
+          {articulos.map(art => {
+            return (
+              <tr key = {art.codigo}>
+                <td>{art.codigo}</td>
+                <td>{art.descripcion}</td>
+                <td>{art.precio}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+      <button onClick = {eliminarUltimaFila}>Eliminar Fila</button>
     </div>
   );
 }
