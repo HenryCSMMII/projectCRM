7bab258c6b21723ebb3176a67d75087788fde71f
HenryCS <henryca8@hotmail.com>
2024-08-18 19:46:35 -0500

    Objetos y constantes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
+function retornarNumeroAleatorio(){
+  return Math.trunc(Math.random()*10);
+}
+
 function App() {
+  const siglo = 21;
+  const persona = {
+    nombre: "Juan",
+    edad: '18'
+  }
   return (
     // <div className="App">
     //   <header className="App-header">
@@ -19,7 +28,18 @@ function App() {
     //     </a>
     //   </header>
     // </div>
-    <h1>Hola Mundo</h1>
+    <div>
+      <h1>Titulo nivel 1</h1>
+      <hr/>
+      <p>Estamos en el siglo {siglo}</p>
+      <h3>Acceso a un objeto</h3>
+      <p>{persona.nombre} tiene {persona.edad} años</p>
+      <h3>Llamada a una función</h3>
+      <p>Un valor aleatorio llamando a un método:</p>
+      {retornarNumeroAleatorio()}
+      <h3>Cálculo inmediato de expresiones</h3>
+      2 + 2 = {2+2}
+    </div>
   );
 }
 
