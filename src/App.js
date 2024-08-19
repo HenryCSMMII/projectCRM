15ae3e05270f30440ddb72f1c176a4e3e1d5271b
HenryCS <henryca8@hotmail.com>
2024-08-18 19:55:40 -0500

    Buscadores, parámetros

--- src/App.js
+++ src/App.js
@@ -1,44 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
-function retornarNumeroAleatorio(){
-  return Math.trunc(Math.random()*10);
+function mostrarTitulo(titulo){
+  return (
+    <h1>{titulo}</h1>
+  );
 }
 
 function App() {
-  const siglo = 21;
-  const persona = {
-    nombre: "Juan",
-    edad: '18'
-  }
+  const buscadores = ['http://www.google.com', 'http://www.bing.com', 'http://www.yahoo.com'];
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
     <div>
-      <h1>Titulo nivel 1</h1>
-      <hr/>
-      <p>Estamos en el siglo {siglo}</p>
-      <h3>Acceso a un objeto</h3>
-      <p>{persona.nombre} tiene {persona.edad} años</p>
-      <h3>Llamada a una función</h3>
-      <p>Un valor aleatorio llamando a un método:</p>
-      {retornarNumeroAleatorio()}
-      <h3>Cálculo inmediato de expresiones</h3>
-      2 + 2 = {2+2}
+      {mostrarTitulo('Buscadores')}
+      <a href={buscadores[0]}>Google</a>
+      <br></br>
+      <a href={buscadores[1]}>Bing</a>
+      <br></br>
+      <a href={buscadores[2]}>Yahoo</a>
     </div>
   );
 }
